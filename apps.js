ada86c48759bc58cedc77b06f36fd3e563f0f91d
Kaif <kaifattari0123@gmail.com>
2026-02-21 08:11:29 +0500

    hi

--- apps.js
+++ apps.js
@@ -1,18 +1,66 @@
-let inputfile = document.querySelector("#fi")
+// let inputfile = document.querySelector("#fi")
 
-let btn =document.querySelector("#btn")
+// let btn =document.querySelector("#btn")
 
-window.addEventListener("click", function(){
-    inputfile.click();
+// window.addEventListener("click", function(){
+//     inputfile.click();
     
 
-})
+// })
 
-inputfile.addEventListener("change", function (del) {
-    let file = del.target.files[0]
-    if (file) {
-    btn.textContent = file.name;
+// inputfile.addEventListener("change", function (del) {
+//     let file = del.target.files[0]
+//     if (file) {
+//     btn.textContent = file.name;
      
-    }
+//     }
     
+// })
+
+
+
+let form = document.querySelector("form");
+form.addEventListener("submit", function (del){
+
+    del.preventDefault();
+let inputs = document.querySelectorAll("input")
+
+
+ let main = document.querySelector("main")
+ 
+ main.classList.add('card')
+
+ 
+ let profile = document.createElement("div")
+ profile.classList.add("profile")
+ let img = document.createElement("img")
+ img.setAttribute("src",inputs[0].value)
+profile.appendChild(img)
+
+
+let h3 = document.createElement("h3")
+h3.textContent = inputs[1].value
+
+
+let h4 = document.createElement("h4")
+h4.textContent = inputs[2].value
+
+
+let p = document.createElement("p")
+p.textContent = inputs[3].value
+    
+
+main.appendChild(profile)
+main.appendChild(h3)
+main.appendChild(h4)
+main.appendChild(p)
+
+
+inputs.forEach(function (inp) {
+if (inp.type !== "submit") {
+    inp.value = ""
+    
+}
 })
+})
+ 
