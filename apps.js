d4f66dc4e368ba22afeabc410edc6f07a031312a
Kaif <kaifattari0123@gmail.com>
2026-02-22 23:28:16 +0500

    gg

--- apps.js
+++ apps.js
@@ -19,48 +19,56 @@
 
 
 
-let form = document.querySelector("form");
-form.addEventListener("submit", function (del){
+// let form = document.querySelector("form");
+// form.addEventListener("submit", function (del){
 
-    del.preventDefault();
-let inputs = document.querySelectorAll("input")
+//     del.preventDefault();
+// let inputs = document.querySelectorAll("input")
 
 
- let main = document.querySelector("main")
+//  let main = document.querySelector("main")
  
- main.classList.add('card')
+//  main.classList.add('card')
 
  
- let profile = document.createElement("div")
- profile.classList.add("profile")
- let img = document.createElement("img")
- img.setAttribute("src",inputs[0].value)
-profile.appendChild(img)
+//  let profile = document.createElement("div")
+//  profile.classList.add("profile")
+//  let img = document.createElement("img")
+//  img.setAttribute("src",inputs[0].value)
+// profile.appendChild(img)
 
 
-let h3 = document.createElement("h3")
-h3.textContent = inputs[1].value
+// let h3 = document.createElement("h3")
+// h3.textContent = inputs[1].value
 
 
-let h4 = document.createElement("h4")
-h4.textContent = inputs[2].value
+// let h4 = document.createElement("h4")
+// h4.textContent = inputs[2].value
 
 
-let p = document.createElement("p")
-p.textContent = inputs[3].value
+// let p = document.createElement("p")
+// p.textContent = inputs[3].value
     
 
-main.appendChild(profile)
-main.appendChild(h3)
-main.appendChild(h4)
-main.appendChild(p)
+// main.appendChild(profile)
+// main.appendChild(h3)
+// main.appendChild(h4)
+// main.appendChild(p)
 
 
-inputs.forEach(function (inp) {
-if (inp.type !== "submit") {
-    inp.value = ""
+// inputs.forEach(function (inp) {
+// if (inp.type !== "submit") {
+//     inp.value = ""
     
-}
-})
-})
+// }
+// })
+// })
  
+let abcd = document.querySelector("#abcd")
+
+window.addEventListener("mousemove" , function (detls) {
+    console.log(detls)
+    let yval = abcd.style.top = detls.clientY - 50+ "px"
+     let xval = abcd.style.left = detls.clientX - 50 + "px"
+    
+})
